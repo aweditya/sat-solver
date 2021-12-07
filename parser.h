ceee6736bf01d5feaf0d568f173ae0ca4863e7d6
Aditya Sriram <aweditya@gmail.com>
2021-12-07 12:26:42 +0530

    Made clauses public instead of private

--- parser.h
+++ parser.h
@@ -8,13 +8,15 @@ class Parser
 {
 private:
     int n_vars, n_clauses;
-    std::vector<std::vector<int>> clauses;
+    
     std::string trim(const std::string s);
     int stringToInt(const std::string number);
     void processProblemLine(std::string line, int &n_vars, int &n_clauses);
     void processClauseLine(std::string line);
     
 public:
+    std::vector<std::vector<int>> clauses;
+
     Parser(std::string path);
     int getNumberOfClauses();
     int getNumberOfVariables();
