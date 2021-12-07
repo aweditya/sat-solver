11b48eeaf88d8e7307770218c9fd6300c26283a5
Aditya Sriram <aweditya@gmail.com>
2021-12-07 11:54:20 +0530

    Added header for Parser

---
+++ parser.h
@@ -0,0 +1,24 @@
+# ifndef _PARSER_
+# define _PARSER_
+
+#include <vector>
+#include <string>
+
+class Parser
+{
+private:
+    int n_vars, n_clauses;
+    std::vector<std::vector<int>> clauses;
+    std::string trim(const std::string s);
+    int stringToInt(const std::string number);
+    void processProblemLine(std::string line, int &n_vars, int &n_clauses);
+    void processClauseLine(std::string line);
+    
+public:
+    Parser(std::string path);
+    int getNumberOfClauses();
+    int getNumberOfVariables();
+    void printClauses();
+};
+
+# endif
